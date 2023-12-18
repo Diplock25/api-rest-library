ff2c8c07fd7d2551e7ebbc82b4f3f0bf5697a9c2
rubenquintas <rubenquintasnogueiras@gmail.com>
2023-12-18 20:06:30 +0100

    feat: esto es una prueba (#1)

--- src/main/java/com/diplock/library/entities/User.java
+++ src/main/java/com/diplock/library/entities/User.java
@@ -1,7 +1,16 @@
 package com.diplock.library.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -12,23 +21,23 @@ import lombok.*;
 @Table(name = "user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_user")
-    private Long id_user;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_user")
+  private Long id_user;
 
-    @Column(name = "id_role", nullable = false)
-    private Long id_role;
+  // esto es un comentario
+  @Column(name = "id_role", nullable = false)
+  private Long id_role;
 
-    @Column(name = "name_user", nullable = false, length = 25)
-    private String name_user;
+  @Column(name = "name_user", nullable = false, length = 25)
+  private String name_user;
 
-    @Column(name = "email", unique = true, nullable = false)
-    private String email;
+  @Column(name = "email", unique = true, nullable = false)
+  private String email;
 
-    @Column(name = "password",nullable = false)
-    private String password;
-
+  @Column(name = "password", nullable = false)
+  private String password;
 
 
 }
